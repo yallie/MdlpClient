eccc345c1fcc8395f833169b94faeeadd76f98ac
Alexey Yakovlev <yallie@yandex.ru>
2020-05-10 19:43:45 +0300

    Exit with dotnet test exit code.

--- Scripts/Coverage.ps1
+++ Scripts/Coverage.ps1
@@ -1,7 +1,11 @@
 ﻿# run unit tests and gather coverage information
 $args = "test --logger=trx;LogFileName=TestResults.trx"
 $filter = "+[*]* -[*Test*]*"
-OpenCover.Console.exe -register:administrator -target:dotnet.exe "-targetargs:$args" "-filter:$filter" -output:MdlpCoverage.xml
+OpenCover.Console.exe -returntargetcode -register:administrator -target:dotnet.exe "-targetargs:$args" "-filter:$filter" -output:MdlpCoverage.xml
+$exit = $lastexitcode
 
 # upload reports to the codecov.io server
 codecov -t "064fad14-0241-4b7a-9c86-ab7b5ed067a3" -f MdlpCoverage.xml 
+
+# return dotnet test exit code
+exit $exit
