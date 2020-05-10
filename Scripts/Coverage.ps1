8a49e7b4ae2346a02fd6deb7c5de7e39bd3ad6de
Alexey Yakovlev <yallie@yandex.ru>
2020-05-10 19:07:47 +0300

    More quotes for OpenCover arguments.

--- Scripts/Coverage.ps1
+++ Scripts/Coverage.ps1
@@ -1,5 +1,7 @@
 ﻿# run unit tests and gather coverage information
-OpenCover.Console.exe -register:administrator -target:dotnet.exe -targetargs:"test --logger=trx;LogFileName=TestResults.trx" -filter:"+[*]* -[*Test*]*" -output:MdlpCoverage.xml
+$args = "test --logger=trx;LogFileName=TestResults.trx"
+$filter = "+[*]* -[*Test*]*"
+OpenCover.Console.exe -register:administrator -target:dotnet.exe "-targetargs:$args" "-filter:$filter" -output:MdlpCoverage.xml
 
 # upload reports to the codecov.io server
 codecov -t "064fad14-0241-4b7a-9c86-ab7b5ed067a3" -f MdlpCoverage.xml 
