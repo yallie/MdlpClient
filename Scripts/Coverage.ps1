2ba50c04a7d94be71ba54c3959ce7e589afbb330
Alexey Yakovlev <yallie@yandex.ru>
2020-05-10 18:59:40 +0300

    Added powershell script to work around the OpenCover command line issue.

---
+++ Scripts/Coverage.ps1
@@ -0,0 +1,5 @@
+﻿# run unit tests and gather coverage information
+OpenCover.Console.exe -register:administrator -target:dotnet.exe -targetargs:"test --logger=trx;LogFileName=TestResults.trx" -filter:"+[*]* -[*Test*]*" -output:MdlpCoverage.xml
+
+# upload reports to the codecov.io server
+codecov -t "064fad14-0241-4b7a-9c86-ab7b5ed067a3" -f MdlpCoverage.xml 
