d6ae7af0162001d138064e4bfcbc6113b9d84b7f
Alexey Yakovlev <yallie@yandex.ru>
2020-05-10 19:58:14 +0300

    Run trx2junit before failing in the hope to see test reports on Gitlab.

--- Scripts/Coverage.ps1
+++ Scripts/Coverage.ps1
@@ -7,5 +7,8 @@ $exit = $lastexitcode
 # upload reports to the codecov.io server
 codecov -t "064fad14-0241-4b7a-9c86-ab7b5ed067a3" -f MdlpCoverage.xml 
 
+# convert trx reports to JUnit format so Gitlab can parse them
+trx2junit MdlpApiClient.Tests\TestResults\TestResults.trx
+
 # return dotnet test exit code
 exit $exit
