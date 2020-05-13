56494383d6ce8c84592b63395d24e1130d6cc48c
yallie <yallie@yandex.ru>
2020-05-14 01:02:28 +0300

    Experimenting with github mirror...

---
+++ Scripts/Mirror.ps1
@@ -0,0 +1,40 @@
+﻿# Push changes to the public mirror
+
+param (
+	[parameter(Mandatory=$true)] $dir,
+	[parameter(Mandatory=$true)] $repo,
+	[parameter(Mandatory=$true)] $url,
+	[parameter(Mandatory=$true)] $user,
+	[parameter(Mandatory=$true)] $remote,
+	[parameter(Mandatory=$true)] $token
+)
+
+# create mirror if not exists
+if (-not (Test-Path $dir))
+{
+	echo "Mirror directory doesn't exits, create $dir..."
+	md $dir
+	pushd $dir
+
+	echo "Cloning..."
+	git clone --mirror $repo .
+
+	echo "Building url..."
+	$uri = [System.UriBuilder]$url
+	$uri.UserName = $user
+	$uri.Password = $token
+	$url = $uri.ToString()
+
+	echo "Adding new remote..."
+	git remote add $remote $url
+	popd
+}
+
+# update mirror and push to remote
+pushd $dir
+echo "Fetching updates to the mirror..."
+git fetch
+
+echo "Pushing to $remote..."
+git push $remote -n
+popd
