f5a08e15b7cbe33afb2f90b699ea903ad2ba9717
Alexey Yakovlev <yallie@yandex.ru>
2020-05-15 16:53:52 +0300

    Added issue test for support ticket #SR00497874.

---
+++ MdlpApiClient.Tests/ApiTestsMisc.cs
@@ -0,0 +1,48 @@
+﻿namespace MdlpApiClient.Tests
+{
+    using System;
+    using MdlpApiClient.DataContracts;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ApiTestsMisc : UnitTestsClientBase
+    {
+        protected override MdlpClient CreateClient()
+        {
+            return new MdlpClient(credentials: new ResidentCredentials
+            {
+                ClientID = ClientID1,
+                ClientSecret = ClientSecret1,
+                UserID = TestUserThumbprint,
+            },
+            baseUrl: MdlpClient.StageApiHttps)
+            {
+                Tracer = WriteLine
+            };
+        }
+
+        /// <summary>
+        /// Ошибка скачивания документа по идентификатору
+        /// </summary>
+        [Test]
+        public void IssueTestSR00497874()
+        {
+            // 1. получаем список входящих документов
+            var docs = Client.GetIncomeDocuments(new DocFilter
+            {
+                DocType = 607,
+                DocStatus = DocStatusEnum.PROCESSED_DOCUMENT,
+                ProcessedDateFrom = DateTime.Now.AddYears(-100)
+            }, 0, 1);
+            Assert.NotNull(docs);
+            Assert.NotNull(docs.Documents);
+            Assert.AreEqual(1, docs.Documents.Length);
+
+            // 2. скачиваем первый документ из списка, получаем ошибку
+            var docId = docs.Documents[0].DocumentID;
+            Assert.IsFalse(string.IsNullOrWhiteSpace(docId));
+            var doc = Client.GetDocumentText(docId);
+            Assert.NotNull(doc);
+        }
+    }
+}
