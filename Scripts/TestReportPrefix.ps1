238753eceaf46c0b645158d252234a644499d32a
Alexey Yakovlev <yallie@yandex.ru>
2020-06-04 01:31:08 +0300

    Add prefixes to the unit tests to distinguish between normal and merged versions.
    Normal version uses ServiceStack.Text v4 and merged one uses ServiceStack.Text v5.

---
+++ Scripts/TestReportPrefix.ps1
@@ -0,0 +1,35 @@
+﻿# Add prefix to the test fixture names
+# This step is necessary because GitLab seems to
+# merge test reports based on their class names
+#
+# Usage:
+#
+# powershell /file TestReportPrefix.ps1 -inputFileName in.xml -outputFileName out.xml -prefix MyProject
+
+using assembly System.Xml.Linq
+using namespace System.Xml.Linq
+
+param (
+	[parameter(Mandatory = $true)] $inputFileName, 
+	[parameter(Mandatory = $true)] $outputFileName, 
+	[parameter(Mandatory = $true)] $prefix
+)
+
+echo "Loading $inputFileName..."
+$xml = [XDocument]::Load($inputFileName)
+
+# add prefix to all test suite class names
+ForEach ($suite in $xml.Root.Elements("testsuite")) 
+{
+	ForEach ($case in $suite.Elements("testcase"))
+	{
+		$name = $case.Attribute("classname")
+		if ($name -ne $null)
+		{
+			$name.Value = $prefix + "." + $name.Value
+		}
+	}
+}
+
+echo "Saving $outputFileName..."
+$xml.Save($outputFileName)
